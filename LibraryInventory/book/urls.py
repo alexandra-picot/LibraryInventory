4896a2f029678de12e401ea022d718c4c05763a0
hafsa-hussain <gh7070@wayne.edu>
2019-03-28 16:52:58 -0400

    add password

--- LibraryInventory/book/urls.py
+++ LibraryInventory/book/urls.py
@@ -5,5 +5,6 @@ app_name = 'book'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('catalog/', views.CatalogView.as_view(), name='catalog')
+    path('catalog/', views.CatalogView.as_view(), name='catalog'),
+    path('details/', views.DetailsView.as_view(), name='details')
 ]
