37529ce627e33149a91df26ef17136c99621b5a5
hafsa-hussain <gh7070@wayne.edu>
2019-04-13 18:14:20 -0400

    removed queryset function

--- LibraryInventory/book/views/viewDetails.py
+++ LibraryInventory/book/views/viewDetails.py
@@ -5,8 +5,3 @@ class DetailsView(generic.DetailView):
     model = Book
     template_name = 'book/details.html'
 
-    def get_queryset(self):
-        """
-        Returns all details regarding Book title chosen.
-        """
-        return Book.object.filter(id=id)
