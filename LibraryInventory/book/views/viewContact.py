d832e33144474ed1493cb9118b669e7d7ca0531f
hafsa-hussain <gh7070@wayne.edu>
2019-04-18 18:36:11 -0400

    exception blocks checking that no number has been entered as a name.

--- LibraryInventory/book/views/viewContact.py
+++ LibraryInventory/book/views/viewContact.py
@@ -1,5 +1,25 @@
-from django.views import generic
+from django.views import View
+from django.shortcuts import render
+from book.utils import FormException
 
 
-class ContactView(generic.TemplateView):
-    template_name = 'book/contact.html'
+class ContactView(View):
+    def get(self, request):
+        return render(request, 'book/contact.html')
+
+    def post(self, request):
+        try:
+            resp = request.POST
+            first_name = resp.get("myFName")
+            last_name = resp.get("myLName")
+            print(first_name)
+            if not first_name.isalpha():
+                raise FormException({"error": "Invalid First Name."})
+            if not last_name.isalpha():
+                raise FormException({"error": "Invalid Last Name"})
+            return render(request, 'book/thanks.html')
+        except FormException as error:
+            print(error.args[0])
+            return render(request, 'book/contact.html', {'input_error': error.args[0]["error"]})
+
+
