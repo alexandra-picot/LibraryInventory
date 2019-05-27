866a264937ed193e72e125675a504a13d528a8c6
Vincent PICOT <vincent.picot.57@gmail.com>
2019-05-27 22:34:36 +0200

    Remove WSU content that will be useless for now on

--- TestDjango/testProject/polls/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from polls.views.viewDetail import DetailView
-from polls.views.viewIndex import IndexView
-from polls.views.viewResults import ResultsView
-from polls.views.viewVote import vote
--- TestDjango/testProject/polls/views/viewResults.py
+++
@@ -1,7 +0,0 @@
-from django.views import generic
-from polls.models import Question
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
